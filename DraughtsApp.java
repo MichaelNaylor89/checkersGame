9d06802314ece857d5110ba985eb7e0cf35083d7
Michael Naylor <michael.t.naylor@stu.mmu.ac.uk>
2017-02-10 17:09:35 +0000

    updated game
    added ability to jump over enemy pieces to take them and victory
    conditions

--- DraughtsApp.java
+++ DraughtsApp.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class DraughtsApp
 {
-	@SuppressWarnings("resource")
+
 	public static void main (String [] args)
 	{
 		DraughtsBoard GameBoard = new DraughtsBoard();
@@ -10,95 +10,257 @@ public class DraughtsApp
 		
 		final int BLANK = 0;
     	final int BLACK = 1;
-    	final int WHITE = 2; 
+    	final int WHITE = 2;
+
+        int selectedPiece = 0;
+        int jumpedPiece = 0;
     	
 		int X, Y;
-		int Piece = 0;
+		int Piece;
 		X=0;
 		Y=0;
-		int newX = 0;
-		int newY = 0;
-		int newSpace = 0;
+		int newX;
+		int newY;
+        int jpX;
+        int jpY;
+		int newSpace;
 		int turn = 1;
-		
-			while (X != -1)
-			{		
-				System.out.println();
-				GameBoard.draw();
-				System.out.println("\nEnter -1 to quit");
-				System.out.print("Enter X coordinate: ");
-				X = in.nextInt();
-				if (X != -1)
-				{
-					System.out.print("Enter Y coordinate: ");
-					Y = in.nextInt();
-					
-					Piece = GameBoard.get_square(X-1, Y-1);
-					System.out.print("\nSquare at "+X+","+Y+" is ");
-					switch (Piece)
-					{
-						case BLANK:
-							System.out.println("blank");
-							break;
-						
-						case BLACK:
-							System.out.println("a black piece");
-							break;
-						
-						case WHITE:
-							System.out.println("a white piece");
-							break;
-					}
-				}
-			
-				newX = 0;
-				newY = 0;
-				System.out.println("Enter a new X coordinate: ");
-				while(!(newX > 0 && newX < 9))
-				{
-					newX = in.nextInt();
-				}
-				
-				System.out.println("Enter a new Y coordinate: ");
-				
-				while(!(newY > 0 && newY < 9))
-				{
-					newY = in.nextInt();
-				}
-				
-				newSpace = GameBoard.get_square(newX-1, newY-1);
-				
-				if(newSpace == WHITE)
-				{
-					System.out.println("This space is already occupied by a white piece");
-				}
-				else if(newSpace == BLACK)
-				{
-					System.out.println("This space is already occupied by a black piece");
-				}
-				else if(newSpace == BLANK)
-				{
-					if(newY - Y == 1 && newX - X == 1)
-					{
-						GameBoard.set_square(newX-1, newY-1, GameBoard.get_square(X-1, Y-1));
-						GameBoard.set_square(X-1, Y-1, BLANK);
-					}
-					else if(newY - Y == 1 && newX - X == -1)
-					{
-						GameBoard.set_square(newX-1, newY-1, GameBoard.get_square(X-1, Y-1));
-						GameBoard.set_square(X-1, Y-1, BLANK);
-						
-					}
-					else
-					{
-						System.out.println("that move is not valid");
-					}
-					
-				}
-				
-				turn++;
-				System.out.println("turn: " + turn);
-	
-			}
+        int whitePiecesRemaining = 12;
+        int blackPiecesRemaining = 12;
+
+        boolean moved = false;
+
+        if (blackPiecesRemaining > 0 && whitePiecesRemaining > 0) {
+            if (blackPiecesRemaining <= 0)
+            {
+                System.out.println ("White Wins");
+            }
+            else if (whitePiecesRemaining <= 0)
+            {
+                System.out.println("Black Wins");
+            }
+            else {
+
+                while (X != -1) {
+                    System.out.println();
+                    GameBoard.draw();
+                    if (turn % 2 == 1)
+                    {
+                        System.out.println("Whites Move");
+                    }
+                    else
+                    {
+                        System.out.println("Blacks Move");
+                    }
+                    System.out.println("\nEnter -1 to quit");
+                    System.out.print("Enter X coordinate: ");
+                    X = in.nextInt();
+                    if (X != -1) {
+                        System.out.print("Enter Y coordinate: ");
+                        Y = in.nextInt();
+
+                        Piece = GameBoard.get_square(X - 1, Y - 1);
+                        System.out.print("\nSquare at " + X + "," + Y + " is ");
+                        switch (Piece) {
+                            case BLANK:
+                                System.out.println("blank");
+                                selectedPiece = 0;
+                                break;
+
+                            case BLACK:
+                                System.out.println("a black piece");
+                                selectedPiece = 1;
+                                break;
+
+                            case WHITE:
+                                System.out.println("a white piece");
+                                selectedPiece = 2;
+                                break;
+                        }
+                    }
+
+                    if (turn % 2 == 1 && selectedPiece == 2) {
+                        newX = 0;
+                        newY = 0;
+                        jpX = 0;
+                        jpY = 0;
+                        System.out.println("Enter a new X coordinate: ");
+                        while (!(newX > 0 && newX < 9)) {
+                            newX = in.nextInt();
+                        }
+
+                        System.out.println("Enter a new Y coordinate: ");
+
+                        while (!(newY > 0 && newY < 9)) {
+                            newY = in.nextInt();
+                        }
+
+                        newSpace = GameBoard.get_square(newX - 1, newY - 1);
+
+                        if (newSpace == WHITE)
+                        {
+                            System.out.println("This space is already occupied by a white piece");
+                        }
+                        else if (newSpace == BLACK)
+                        {
+                            System.out.println("This space is already occupied by a black piece");
+                        }
+                        else if (newSpace == BLANK)
+                        {
+                            if (newY - Y == 1 && newX - X == 1)
+                            {
+                                GameBoard.set_square(newX - 1, newY - 1, GameBoard.get_square(X - 1, Y - 1));
+                                GameBoard.set_square(X - 1, Y - 1, BLANK);
+                                moved = true;
+                            }
+                            else if (newY - Y == 1 && newX - X == -1)
+                            {
+                                GameBoard.set_square(newX - 1, newY - 1, GameBoard.get_square(X - 1, Y - 1));
+                                GameBoard.set_square(X - 1, Y - 1, BLANK);
+                                moved = true;
+                            }
+                            else if (newY - Y == 2 && newX - X == 2)
+                            {
+                                jpX = ((X - newX) / 2) + newX;
+                                jpY = ((Y - newY) / 2) + newY;
+                                jumpedPiece = GameBoard.get_square(jpX - 1, jpY - 1);
+
+                                if (jumpedPiece == BLACK) {
+                                    GameBoard.set_square(newX - 1, newY - 1, GameBoard.get_square(X - 1, Y - 1));
+                                    GameBoard.set_square(X - 1, Y - 1, BLANK);
+                                    GameBoard.set_square(jpX - 1, jpY - 1, BLANK);
+                                    blackPiecesRemaining -= 1;
+                                    System.out.println("black piece taken " + blackPiecesRemaining + " black pieces remaining");
+                                    moved = true;
+                                }
+                                else
+                                {
+                                    System.out.println("this is not a valid move");
+                                }
+                            }
+                            else if (newY - Y == 2 && newX - X == -2)
+                            {
+                                jpX = ((X - newX) / 2) + newX;
+                                jpY = ((Y - newY) / 2) + newY;
+                                jumpedPiece = GameBoard.get_square(jpX - 1, jpY - 1);
+                                if (jumpedPiece == BLACK) {
+                                    GameBoard.set_square(newX - 1, newY - 1, GameBoard.get_square(X - 1, Y - 1));
+                                    GameBoard.set_square(X - 1, Y - 1, BLANK);
+                                    GameBoard.set_square(jpX - 1, jpY - 1, BLANK);
+                                    blackPiecesRemaining -= 1;
+                                    System.out.println("Black piece taken " + blackPiecesRemaining + " black pieces remaining");
+                                    moved = true;
+                                }
+                                else
+                                {
+                                    System.out.println("this is not a valid move");
+                                }
+                            }
+
+                            else
+                            {
+                                System.out.println("that move is not valid");
+                            }
+                        }
+                        if (moved == true) {
+                            turn++;
+                            System.out.println("turn: " + turn);
+                            moved = false;
+                        }
+                    }
+                    else if (turn % 2 == 0 && selectedPiece == 1)
+                    {
+                        newX = 0;
+                        newY = 0;
+                        jpX = 0;
+                        jpY = 0;
+                        System.out.println("Enter a new X coordinate: ");
+                        while (!(newX > 0 && newX < 9)) {
+                            newX = in.nextInt();
+                        }
+                        System.out.println("Enter a new Y coordinate: ");
+
+                        while (!(newY > 0 && newY < 9)) {
+                            newY = in.nextInt();
+                        }
+                        newSpace = GameBoard.get_square(newX - 1, newY - 1);
+
+                        if (newSpace == WHITE)
+                        {
+                            System.out.println("This space is already occupied by a white piece");
+                        }
+                        else if (newSpace == BLACK)
+                        {
+                            System.out.println("This space is already occupied by a black piece");
+                        }
+                        else if (newSpace == BLANK)
+                        {
+                            if (newY - Y == -1 && newX - X == 1) {
+                                GameBoard.set_square(newX - 1, newY - 1, GameBoard.get_square(X - 1, Y - 1));
+                                GameBoard.set_square(X - 1, Y - 1, BLANK);
+                                moved = true;
+                            }
+                            else if (newY - Y == -1 && newX - X == -1)
+                            {
+                                GameBoard.set_square(newX - 1, newY - 1, GameBoard.get_square(X - 1, Y - 1));
+                                GameBoard.set_square(X - 1, Y - 1, BLANK);
+                                moved = true;
+                            }
+                            else if (newY - Y == -2 && newX - X == 2)
+                            {
+                                jpX = ((X - newX) / 2) + newX;
+                                jpY = ((Y - newY) / 2) + newY;
+                                jumpedPiece = GameBoard.get_square(jpX - 1, jpY - 1);
+                                if (jumpedPiece == WHITE)
+                                {
+                                    GameBoard.set_square(newX - 1, newY - 1, GameBoard.get_square(X - 1, Y - 1));
+                                    GameBoard.set_square(X - 1, Y - 1, BLANK);
+                                    GameBoard.set_square(jpX - 1, jpY - 1, BLANK);
+                                    whitePiecesRemaining -= 1;
+                                    System.out.println("White piece taken " + whitePiecesRemaining + " white pieces remaining");
+                                    moved = true;
+                                }
+                                else
+                                {
+                                    System.out.println("this is not a valid move");
+                                }
+                            }
+                            else if (newY - Y == -2 && newX - X == -2)
+                            {
+                                jpX = ((X - newX) / 2) + newX;
+                                jpY = ((Y - newY) / 2) + newY;
+                                jumpedPiece = GameBoard.get_square(jpX - 1, jpY - 1);
+                                if (jumpedPiece == WHITE)
+                                {
+                                    GameBoard.set_square(newX - 1, newY - 1, GameBoard.get_square(X - 1, Y - 1));
+                                    GameBoard.set_square(X - 1, Y - 1, BLANK);
+                                    GameBoard.set_square(jpX - 1, jpY - 1, BLANK);
+                                    whitePiecesRemaining -= 1;
+                                    System.out.println("white piece taken " + whitePiecesRemaining + " white pieces remaining");
+                                    moved = true;
+                                }
+                                else
+                                {
+                                    System.out.println("this is not a valid move");
+                                }
+                            }
+                            else
+                            {
+                                System.out.println("that move is not valid");
+                            }
+                        }
+                        if (moved == true) {
+                            turn++;
+                            System.out.println("turn: " + turn);
+                            moved = false;
+                        }
+                    }
+                    else
+                    {
+                        System.out.println("Please select one of your pieces");
+                    }
+                }
+            }
+        }
 	}
 }
